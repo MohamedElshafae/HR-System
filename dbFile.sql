d2be1605c1967f77498cbbe0feb8473d5dbeb65f
OMNIA <101533216+omniamagdyy@users.noreply.github.com>
2024-07-30 16:12:48 +0300

    Update dbFile.sql

--- dbFile.sql
+++ dbFile.sql
@@ -1,50 +1,61 @@
-CREATE DATABASE IF NOT EXISTS hrSystem;
+CREATE DATABASE IF NOT EXISTS hr;
 
-USE hrSystem;
+USE hr;
 
-CREATE TABLE IF NOT EXISTS Departments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    location VARCHAR(255)
+CREATE TABLE Departments (
+    DepartmentID INT AUTO_INCREMENT PRIMARY KEY,
+    DepartmentName VARCHAR(100) NOT NULL 
 );
 
-CREATE TABLE IF NOT EXISTS Jobs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description text
+CREATE TABLE Roles (
+    RoleID int  PRIMARY KEY,
+    RoleTitle VARCHAR(100) NOT NULL
+    
 );
 
-CREATE TABLE IF NOT EXISTS Employees (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    gender VARCHAR(10),
-    job_id INT,
-    email_address VARCHAR(255),
-    phone_number VARCHAR(20),
-    address VARCHAR(255),
-    department_id INT,
-    manager_id iNT,
-    FOREIGN KEY (department_id) REFERENCES Departments(id),
-    FOREIGN KEY (job_id) REFERENCES Jobs(id),
-    FOREIGN KEY (manager_id) REFERENCES Employees(id)
+create TABLE Employee (
+    EmployeeID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Phone VARCHAR(15),
+    NationalID VARCHAR(20) UNIQUE,
+    DepartmentID INT,
+    RoleID INT,
+    HireDate DATE,
+    DateOfBirth DATE,
+    Gender varchar(50),
+    Address varchar(50),
+    ManagerID INT,
+    FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID) ON DELETE CASCADE,
+    FOREIGN KEY (RoleID) REFERENCES Roles(RoleID)ON DELETE CASCADE ,
+    FOREIGN KEY (ManagerID) REFERENCES Employee(EmployeeID) ON DELETE CASCADE
+    
 );
 
 
 
-CREATE TABLE IF NOT EXISTS AttachmentTypes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+CREATE TABLE  AttachmentsType (
+    AttachmentTypeID INT  PRIMARY KEY,
+    TypeName varchar(255) NOT NULL DEFAULT 'DefaultType'
 );
 
-CREATE TABLE IF NOT EXISTS Attachments (
-    attachment_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    upload_date DATE,
-    file_path VARCHAR(255),
-    employee_id INT,
-    type_id INT,
-    FOREIGN KEY (employee_id) REFERENCES Employees(id),
-    FOREIGN KEY (type_id) REFERENCES AttachmentTypes(id)
+CREATE TABLE  Attachments (
+    AttachmentID INT  PRIMARY KEY,
+    UploadedDate DATE,
+    FilePath VARCHAR(255),
+    EmployeeID INT,
+    AttachmentTypeID INT,
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID) ON DELETE CASCADE,
+    FOREIGN KEY (AttachmentTypeID) REFERENCES AttachmentsType(AttachmentTypeID) ON DELETE CASCADE
+);
+
+CREATE TABLE User (
+    UserName VARCHAR(100) NOT NULL PRIMARY KEY,
+    Password VARCHAR(100) NOT NULL,
+    EmployeeID INT,
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID) ON DELETE CASCADE
+
 );
 
 
