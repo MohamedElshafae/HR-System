af4d5f510689d03b41c1cd267343ca9a9ae1e8e7
Mohamed Elshfae <mohamedelshafae888@gmail.com>
2024-08-06 11:28:53 +0300

    add job service

---
+++ HR-System/HR-System.Core/Interfaces/IJobRepository.cs
@@ -0,0 +1,16 @@
+﻿using HR_System.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_System.Core.Interfaces
+{
+    public interface IJobRepository
+    {
+        Task CreateJob(Job Job);
+        Task<bool> isExist(string jobTitle);
+        Task<IEnumerable<Job>> GetJobs();
+    }
+}
---
+++ HR-System/HR-System.EF/Repositories/JobRepository.cs
@@ -0,0 +1,30 @@
+﻿using HR_System.Core.Interfaces;
+using HR_System.Core.Models;
+using HR_System.EF.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_System.EF.Repositories
+{
+    public class JobRepository : IJobRepository
+    {
+        private readonly HrContext _context;
+        public JobRepository(HrContext context) =>
+            _context = context;
+        public async Task CreateJob(Job job)
+        {
+            await _context.AddAsync(job);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Job>> GetJobs() =>
+            _context.Jobs;
+
+        public Task<bool> isExist(string jobTitle) =>
+            _context.Jobs.AnyAsync(j => j.JobTitle == jobTitle);
+    }
+}
