2d6b0dad6789d41548b13a708b219452937f94f3
Mohamed Elshfae <mohamedelshafae888@gmail.com>
2024-08-06 16:37:18 +0300

    mkae UploadHandlerService

--- HR-System/HR-System.Core/Models/Attachment.cs
+++ HR-System/HR-System.Core/Models/Attachment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HR_System.Core.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
---
+++ HR-System/HR-System.Core/Services/UploadHandlerService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Runtime.CompilerServices;
+namespace HR_System.Core.Services
+{
+    public enum FileType
+    {
+        CV,
+        Img,
+        txt,
+        PDF,
+        docx
+    }
+    public static class UploadHandlerService
+    {
+        public async static Task<bool> UploadFileAsync(IFormFile file, string empName, string empId, FileType fileType)
+        {
+            string fileName = empName + "_" + empId + "_" + fileType + Path.GetExtension(file.FileName);
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Attachments", fileName);
+
+            if (File.Exists(filePath))
+                return false;
+            FileStream stream = new FileStream(filePath, FileMode.Create);
+
+            await file.CopyToAsync(stream);
+            stream.Dispose();
+            stream.Close();
+            return true;
+        }
+        public static bool IsBigger(IFormFile file)
+        {
+            long size = file.Length;
+            long MaxmumSize = 8 * 1024 * 1024;
+            if (size > MaxmumSize)
+                return true;
+            return false;
+        }
+    }
+}
