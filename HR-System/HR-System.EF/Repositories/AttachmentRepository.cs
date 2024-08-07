5e80b5f24bdffbe570dc82c108329604292175fa
Mohamed Elshfae <mohamedelshafae888@gmail.com>
2024-08-07 11:51:15 +0300

    upload repository

---
+++ HR-System/HR-System.EF/Repositories/AttachmentRepository.cs
@@ -0,0 +1,27 @@
+﻿using HR_System.Core.Interfaces;
+using HR_System.Core.Models;
+using HR_System.Core.Services;
+using HR_System.EF.Data;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_System.EF.Repositories
+{
+    public class AttachmentRepository: IAttachmentRepository
+    {
+        private readonly HrContext _context;
+        public AttachmentRepository(HrContext context) =>
+            _context = context;
+
+        public async Task CreateAttachmentAsync(Attachment attachment)
+        {
+            _context.Attachments.Add(attachment);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
